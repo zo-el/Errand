d9b9811164ab07a7e1cf08df3242a6f16af47fb8
jackThammer <joelulahanna@gmail.com>
2018-08-08 15:42:42 -0600

    static board and lanes

--- dna/boards/boards.ts
+++ dna/boards/boards.ts
@@ -1,30 +1,52 @@
-function createCoustomRoom(members: string[]): string {
-  members.push(App.Key.Hash)
-  let uuid: string = uuidGenerator();
-  var coustom_room_details: any = {
-    name: uuid
-  }
-  debug("Your Room UUID: " + uuid);
-  let uuid_hash = commit("coustom_room_uuid", uuid);
-  //debug("uuid_hash: " + uuid_hash);
-  commit("coustom_room_link", { Links: [{ Base: App.DNA.Hash, Link: uuid_hash, Tag: "room_uuid" }] });
+// TODO : Built for only one board for now
 
-  let details_hash = commit("coustom_room_details", coustom_room_details);
-  //debug("details_hash: " + details_hash);
-  commit("coustom_room_link", { Links: [{ Base: uuid_hash, Link: details_hash, Tag: "room_details" }] });
-
-  //addMembers(uuid, members);
-
-  return uuid;
+// -----------------------------------------------------------------
+//  The Public Function
+// -----------------------------------------------------------------
+// TEMP : Created to make a dummy board as an anchor for the ver1
+function newBoard({ title, label }) {
+  //const uuid = uuidGenerator();
+  const board = { title, label };
+  commit("board", board);
 }
 
+function newLane({ title, lable }): Hash {
+  const uuid: string = uuidGenerator();
+  const lane = { uuid, title, lable }
+  const hash: string = commit("lane", lane);
+
+  //TEMP :  add to pregenerated Board
+  const BOARD_HASH = makeHash("board", { title: "First_Board", label: "" });
+
+  commit("lane_link", { Links: [{ Base: BOARD_HASH, Link: hash, Tag: "lane_tag" }] });
+  return hash;
+}
+
+function getLanes({ }): GetLinksResponse[] {
+  //TEMP :  add to pregenerated Board
+  const BOARD_HASH = makeHash("board", { title: "First_Board", label: "" });
+
+  const lanes = getLinks(BOARD_HASH, "lane_tag", { Load: true });
+  // let lane_entry = lanes.map((lane) => {
+  //   return lane.Entry;
+  // });
+
+  debug(`All the lanes: ` + JSON.stringify(lanes));
+  return lanes;
+}
+
+function newCard({ title, description, lane_hash }) {
+  debug(lane_hash)
+  const uuid = uuidGenerator();
+  const card = { title, description, uuid };
+  const hash = commit("card", card);
+  commit("card_link", { Links: [{ Base: lane_hash, Link: hash, Tag: "lane_tag" }] });
+  return hash;
+}
 
 //------------------------------
 // Helper Functions
 //------------------------------
-
-//joel ulahahna\
-
 
 //Generates new UUID ()
 function uuidGenerator() {
@@ -33,15 +55,13 @@ function uuidGenerator() {
     return v.toString(16);
   });
 }
-//For Testing
-function getKey() {
-  return App.Key.Hash;
-}
+
 // -----------------------------------------------------------------
-//  The Genesis Function https://developer.holochain.org/genesis
+//  The Genesis Function
 // -----------------------------------------------------------------
 
 function genesis() {
+  newBoard({ title: "First_Board", label: "" })
   return true;
 }
 
@@ -49,68 +69,32 @@ function genesis() {
 //  Validation functions for every change to the local chain or DHT
 // -----------------------------------------------------------------
 
-// Check if the pub_hash is a member of the
-function isValidAdmin(base_hash: string, entry_source: string): boolean {
-  debug("Checking if Agent is an Admin..");
-  //Checking if the Creator is trying to add people to the room
-  let source: any;
-  try {
-    source = get(base_hash, { GetMask: HC.GetMask.Sources });
-  } catch (e) {
-    return false;
-  }
-  //Added the creater of the room as a member of the room
-  if (JSON.stringify(source) === JSON.stringify(entry_source)) {
-    //debug("Adding Room Creator as a member of the room")
-    return true;
-  }
-  //Checking to see if members are trying to add new members to the room
-  let members: any;
-  try {
-    members = getLinks(base_hash, "room_members", { Load: true });
-  } catch (e) {
-    debug("Rooms Dosnt Exist " + e);
-    return false;
-  }
-  let access: boolean = members.some((member) => {
-    return member.Hash == entry_source
-  });
-  return access;
-}
-// Check to validate if the same user that created the message is modifying the message
-function isValidModifier(replaces: string, sources: any): boolean {
-  let old_message: any;
-  try {
-    old_message = get(replaces)
-  } catch (e) {
-    debug("ERROR: isValidModifier() " + e)
-  }
-  if (old_message.author == sources[0])
-    return true;
-  else
-    return false;
-}
 function validateCommit(entryName: any, entry: any, header: any, pkg: any, sources: any): boolean {
-  debug("entry_type:" + entryName + "entry" + JSON.stringify(entry) + "header" + JSON.stringify(header) + "PKG: " + JSON.stringify(pkg) + "sources" + sources);
+  // debug("entry_type:" + entryName + "entry" + JSON.stringify(entry) + "header" + JSON.stringify(header) + "PKG: " + JSON.stringify(pkg) + "sources" + sources);
   return validate(entryName, entry, header, pkg, sources);
 }
 
 function validate(entryName: any, entry: any, header: any, pkg: any, sources: any): boolean {
   switch (entryName) {
-    case "coustom_room_uuid":
+    case "board":
       return true;
-    case "coustom_room_details":
+    case "lane":
       return true;
-    case "coustom_room_link":
+    case "lane_link":
       return true;
-    case "room_to_member_link":
-      return isValidAdmin(entry.Links[0].Base, sources);
-    case "member_to_room_link":
-      //isValidAdmin(entry);
+    case "card":
       return true;
-    case "cr_message":
+    case "card_link":
       return true;
-    case "cr_message_link":
+    default:
+      return false;
+  }
+}
+
+function validateLink(entryName: any, baseHash: any, links: any, pkg: any, sources: any): boolean {
+  //debug("entryName: "+entryName+" baseHash: "+ baseHash+" links: "+ links+" sources: "+ sources);
+  switch (entryName) {
+    case "lane_link":
       return true;
     default:
       return false;
@@ -122,47 +106,21 @@ function validatePut(entryName: any, entry: any, header: any, pkg: any, sources:
 }
 
 function validateMod(entryName: any, entry: any, header: any, replaces: any, pkg: any, sources: any): boolean {
-  debug("entry_type:" + entryName + "entry" + JSON.stringify(entry) + "header" + JSON.stringify(header) + "replaces: " + replaces + "PKG: " + JSON.stringify(pkg) + "sources" + sources);
+  // debug("entry_type:" + entryName + "entry" + JSON.stringify(entry) + "header" + JSON.stringify(header) + "replaces: " + replaces + "PKG: " + JSON.stringify(pkg) + "sources" + sources);
   switch (entryName) {
-    case "cr_message":
-      return isValidModifier(replaces, sources);
+    case "":
+      return false;
     default:
       return false;
   }
 }
 
-function validateDel(entryName: any, hash: any, pkg: any, sources: any): boolean {
-  return false;
-}
+function validateDel(entryName: any, hash: any, pkg: any, sources: any): boolean { return false; }
 
-function validateLink(entryName: any, baseHash: any, links: any, pkg: any, sources: any): boolean {
-  //debug("entryName: "+entryName+" baseHash: "+ baseHash+" links: "+ links+" sources: "+ sources);
-  switch (entryName) {
-    case "coustom_room_link":
-      return true;
-    case "room_to_member_link":
-      return true;
-    case "member_to_room_link":
-      return true;
-    case "cr_message_link":
-      return true;
-    default:
-      return false;
-  }
-}
+function validatePutPkg(entryName) { return null; }
 
-function validatePutPkg(entryName) {
-  return null;
-}
+function validateModPkg(entryName) { return null; }
 
-function validateModPkg(entryName) {
-  return null;
-}
+function validateDelPkg(entryName) { return null; }
 
-function validateDelPkg(entryName) {
-  return null;
-}
-
-function validateLinkPkg(entryName) {
-  return null;
-}
+function validateLinkPkg(entryName) { return null; }
