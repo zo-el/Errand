7b27f8dc56ecd965042330343b8796baef344e43
jackThammer <joelulahanna@gmail.com>
2018-08-10 12:45:40 -0600

    dragable ui | updates to ui

--- ui-src/src/App.js
+++ ui-src/src/App.js
@@ -18,9 +18,10 @@ const handleDragEnd = (cardId, sourceLaneId, targetLaneId) => {
 }
 
 class App extends Component {
-    state = {boardData: {lanes: []}}
+    state = {"boardData": {"lanes": []}};
 
     setEventBus = eventBus => {
+      console.log("setEventBus")
         this.setState({eventBus})
     }
 
@@ -29,62 +30,74 @@ class App extends Component {
         this.setState({boardData: response})
     }
 
-    getBoard() {
+    getBoard = () => {
         return new Promise(resolve => {
             resolve(data)
         })
     }
 
-    completeCard = () => {
-        this.state.eventBus.publish({
-            type: 'ADD_CARD',
-            laneId: 'COMPLETED',
-            card: {id: 'Milk', title: 'Buy Milk', label: '15 mins', description: 'Use Headspace app'}
-        })
-        this.state.eventBus.publish({type: 'REMOVE_CARD', laneId: 'PLANNED', cardId: 'Milk'})
-    }
-
-    addCard = () => {
-        this.state.eventBus.publish({
-            type: 'ADD_CARD',
-            laneId: 'BLOCKED',
-            card: {id: 'Ec2Error', title: 'EC2 Instance Down', label: '30 mins', description: 'Main EC2 instance down'}
-        })
-    }
-
-    shouldReceiveNewData = nextData => {
+    onDataChange = nextData => {
         console.log('New card has been added')
         console.log(nextData)
-    }
+    };
 
-	handleCardAdd = (card, laneId) => {
-		console.log(`New card added to lane ${laneId}`)
-		console.dir(card)
-	}
+  	onCardAdd = (card, laneId) => {
+  		console.log(`ADDING: New card added to lane ${laneId}`)
+  		console.dir(card)
+  	};
+
+    onCardDelete = (cardId, metadata, laneId)=>{
+      //TODO delete callback to HC
+      console.log("DELETE: ",cardId, metadata ,  laneId);
+    };
+
+    onCardClick = (cardId, metadata, laneId)=>{
+      //TODO Expand the Card to see better and edit
+      console.log("CLICK: ",cardId, metadata ,  laneId);
+    };
+
+    // completeCard = () => {
+    //     this.state.eventBus.publish({
+    //         type: 'ADD_CARD',
+    //         laneId: 'COMPLETED',
+    //         card: {id: 'Milk', title: 'Buy Milk', label: '15 mins', description: 'Use Headspace app'}
+    //     })
+    //     this.state.eventBus.publish({type: 'REMOVE_CARD', laneId: 'PLANNED', cardId: 'Milk'})
+    // };
+    //
+    // addCard = () => {
+    //     this.state.eventBus.publish({
+    //         type: 'ADD_CARD',
+    //         laneId: 'BLOCKED',
+    //         card: {id: 'Ec2Error', title: 'EC2 Instance Down', label: '30 mins', description: 'Main EC2 instance down'}
+    //     })
+    // };
 
     render() {
         return (
             <div className="App">
                 <div className="App-header">
-                    <h3>React Trello Demo</h3>
+                <img src="errand.png" height="42" width="42" />
+                    <h1>Errand</h1>
+
                 </div>
                 <div className="App-intro">
-                    <button onClick={this.completeCard} style={{margin: 5}}>
-                        Complete Buy Milk
-                    </button>
-                    <button onClick={this.addCard} style={{margin: 5}}>
-                        Add Blocked
-                    </button>
+
                     <Board
-                        editable
-												onCardAdd={this.handleCardAdd}
-                        data={this.state.boardData}
-                        draggable
-                        onDataChange={this.shouldReceiveNewData}
-                        eventBusHandle={this.setEventBus}
-                        handleDragStart={handleDragStart}
-                        handleDragEnd={handleDragEnd}
-                    />
+                      data={this.state.boardData}
+                      eventBusHandle={this.setEventBus}
+                      draggable={true}
+                      // laneDraggable={true} /*default*/
+                      // cardDraggable={true} /*default*/
+                      collapsibleLanes={true}
+                      editable={true}
+                      // handleDragStart={this.handleDragStart}
+                      handleDragEnd={handleDragEnd}
+                      onCardClick={this.onCardClick}
+                      onCardAdd={this.onCardAdd}
+                      onCardDelete={this.onCardDelete}
+                      onDataChange={this.onDataChange}
+                      />
                 </div>
             </div>
         )
